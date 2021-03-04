cd9483eaaaeb86df8cff97e4d96154795e36c6d0
romainpch <romain.pioch@pm.me>
2021-03-04 19:39:53 +0100

    Passage en POO

--- design.py
+++ design.py
@@ -1,52 +1,59 @@
 from math import pi, log10, sqrt
+import numpy as np
+import matplotlib.pyplot as plt
 
+
+# Setting constants
 c = 3.0e8
+eps_ap = 0.51
 
-#Caracteristics of the wave
+
+class Horn() :
+    def __init__(self, A,a,b) :
+        self.A = A
+        self.a = a
+        self.b = b
+        self.compute_carac()
+
+    def __str__(self) :
+        return self.affiche()
+    
+    def compute_carac(self) :
+        self.B = 0.5*(self.b + sqrt(self.b**2 + 8/3*self.A*(self.A-self.a)))
+        self.R_H = self.A*(self.A-self.a)/(3*wavelength)
+        self.L_A = sqrt(self.R_H**2 + (self.B-self.b)**2)
+        self.L_B = sqrt(self.R_H**2 + (self.A-self.a)**2)
+
+        self.Gain = 10*log10(4*pi*eps_ap*self.A*self.B/pow(wavelength,2))
+
+    def test_frequency(self) :
+        lambda_c = 2*self.a 
+        nu_c = c/lambda_c
+        nu_min = 1.25*nu_c
+        nu_max = 1.9*nu_c
+        print("The horn frequency limits are : ", round(nu_min*1.e-6,2), 'Mhz < nu < ',  round(nu_max*1.e-6,2), 'Mhz')
+        if nu_min<freq_wave<nu_max :
+            print('Your frequency nu =', round(freq_wave*1.e-6,2), 'Mhz is OK with those a and b\n')
+        else :
+            print('Your frequency nu =', round(freq_wave*1.e-6,2), "Mhz doesn't belongs to this interval\n")
+
+    def affiche(self) :
+        attr = ['A', 'B', 'a', 'b', 'L_A', 'L_B', 'R_H', 'Gain']
+        form = "{0:12}{1:12}{2:12}{3:12}{4:12}{5:12}{6:12}{7:12}"
+        carac = [str(round(100*self.A,2)), str(round(100*self.B,2)), str(round(100*self.a,2)), str(round(100*self.b,2)), str(round(100*self.L_A,2)), str(round(100*self.L_B,2)), str(round(100*self.R_H,2)), str(round(self.Gain,2))]
+        
+        return('Characteristics of the GalaxyClock : \n' + form.format(*attr)+'\n'+ form.format(*carac))
+
+
+#Wave caracteristics
 freq_wave = 1.4204e9
 wavelength = c/freq_wave
 
-#Definition of the parameters of the horn (in m)
+#Parameters of the horn (in m)
 a = 16.3e-2
 b = 10.5e-2
-A = 60.e-2
+A = 49e-2
 
-def frequency_limits(a, b, nu) :
-    lambda_c = 2*a 
-    nu_c = c/lambda_c
-    nu_min = 1.25*nu_c
-    nu_max = 1.9*nu_c
-    return [nu_min,nu_max]
-
-def test_frequency(nu, f_lim) :
-    nu_min,nu_max = f_lim
-    print("The horn frequency limits are : ", round(nu_min*1.e-6,2), 'Mhz < nu < ',  round(nu_max*1.e-6,2), 'Mhz')
-    if nu_min<nu<nu_max :
-        print('Your frequency nu =', round(nu*1.e-6,2), 'Mhz is OK with those a and b\n')
-    else :
-        print('Your frequency nu =', round(nu*1.e-6,2), "Mhz doesn't belongs to this interval\n")
-
-
-def computeGainDB(A, B, wavelength, eps_ap) :
-    return 10*log10(4*pi*eps_ap*A*B/pow(wavelength,2))
-
-def optimal_B(A, a, b) :
-    return 0.5*(b + sqrt(b**2 + 8/3*A*(A-a)))
-
-def compute_RH(A,a,wavelength) :
-    return A*(A-a)/(3*wavelength)
-
-
-
-
-freq_limits = frequency_limits(a,b, freq_wave)
-test_frequency(freq_wave,freq_limits)
-
-B_opti = optimal_B(A, a, b)
-R_H = compute_RH(A,a,wavelength)
-gain = computeGainDB(A, B_opti, wavelength, 0.51)
-
-print("A =", A*100, "cm\n")
-print('The best B from given A, a & b is B =', round(B_opti*1.e2, 2), 'cm\n')
-print('R_H = ',round(R_H*1.e2,2), 'cm\n')
-print('G =', round(gain,2), 'dB\n')
+Galacticlock = Horn(A,a,b)
+Galacticlock.test_frequency()
+print(Galacticlock)
